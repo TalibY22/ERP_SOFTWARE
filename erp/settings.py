a36d24a21abd809e43bac7a7cd4d2b87cd5ab831
TalibY22 <hzhzhhsj58@gmail.com>
2024-04-06 22:08:47 +0300

    security fix

--- erp/settings.py
+++ erp/settings.py
@@ -140,5 +140,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yakubtalib70@gmail.com'
-EMAIL_HOST_PASSWORD = '123456789talib'
+EMAIL_HOST_USER = ''
